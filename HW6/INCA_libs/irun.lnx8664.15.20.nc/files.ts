ba1b9bcb28bab00c6474df41ef8575bf48c898e2
YCW <brian0978599918@gmail.com>
2022-07-21 11:57:31 +0800

    HW6 finished at 07/21

---
+++ HW6/INCA_libs/irun.lnx8664.15.20.nc/files.ts
@@ -0,0 +1,4 @@
+1658375172 /home/caid023/Verilog_pratice/HW6-1/tsmc13_neg.v
+1658375172 /home/caid023/Verilog_pratice/HW6-1/tb_term.sv
+1658375200 /home/caid023/Verilog_pratice/HW6-1/SME.v
+1658375263 /home/caid023/Verilog_pratice/HW6-1/SME_syn.v
--- HW6/build/INCA_libs/irun.lnx8664.15.20.nc/.timestamp.ts
+++
@@ -1,2 +0,0 @@
-1658313134 /home/caid023/Verilog_pratice/HW6/src/SME.v
-1657781453 /home/caid023/Verilog_pratice/HW6/sim/tb_term.sv
