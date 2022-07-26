7980ab155daf1ec759d0cbd5f2f76b35298dc5ee
YuChengWang <brian0978599918@gmail.com>
2022-07-26 18:06:38 +0800

    HW5 finished

--- HW5/build/INCA_libs/irun.lnx8664.15.20.nc/.timestamp.ts
+++ HW5/build/INCA_libs/irun.lnx8664.15.20.nc/.timestamp.ts
@@ -1,3 +1,5 @@
+1658386788 /home/YuChengWang/Verilog_pratice/HW5/src/LBP.v
+1658386788 /home/YuChengWang/Verilog_pratice/HW5/sim/testfixture.v
 1173075089 /home/nfs_cad/lib/CBDK_IC_Contest_v2.1/Verilog/tsmc13_neg.v
 1657781431 /home/caid023/Verilog_pratice/HW5/sim/testfixture.v
 1657781332 /home/caid023/Verilog_pratice/HW5/syn/LBP_syn.v
--- HW5/build/INCA_libs/irun.lnx8664.15.20.nc/files.ts
+++ HW5/build/INCA_libs/irun.lnx8664.15.20.nc/files.ts
@@ -1,4 +1,3 @@
 1173075089 /home/nfs_cad/lib/CBDK_IC_Contest_v2.1/Verilog/tsmc13_neg.v
-1657781431 /home/caid023/Verilog_pratice/HW5/sim/testfixture.v
-1657781422 /home/caid023/Verilog_pratice/HW5/src/LBP.v
-1657781332 /home/caid023/Verilog_pratice/HW5/syn/LBP_syn.v
+1658386788 /home/YuChengWang/Verilog_pratice/HW5/sim/testfixture.v
+1658386788 /home/YuChengWang/Verilog_pratice/HW5/src/LBP.v
